da00d571c9bce8d3edb14c4f07c1c10cc64cb6e2
Leonardo Teixeira Martins <leonardo.t@escolar.ifrn.edu.br>
2024-07-29 14:28:50 -0300

    Instalado e configurado o Tailwindcss.✨

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,8 @@
-import { Stack } from "expo-router";
+import '../styles/global.css'
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Slot/>
   );
 }
